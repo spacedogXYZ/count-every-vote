802c13e313b3b28b80e47d366f08bc41663741ed
Josh Levinger <josh@levinger.net>
2020-11-03 10:01:27 -0800

    show "Last Updated" on StateData page
    for #3
    
    also make it display more like other pages

--- src/templates/State.Data.tsx
+++ src/templates/State.Data.tsx
@@ -7,17 +7,45 @@ import Section from "@components/Section";
 import StateBar from "../components/chart/State.Bar";
 import StateChart from "../components/chart/State.Chart";
 
+import moment from 'moment';
+
+const Heading = styled.h1`
+  font-size: 48px;
+  font-family: var(--merriweather-font);
+  margin-bottom: 25px;
+  line-height: 1.32;
+  color: var(--theme-ui-colors-primary,#000);
+
+  & a:visited {
+    color: unset;
+  }
+`
+
+const LastUpdated = styled.div`
+  font-size: 18px;
+  color: #73737D;
+`
+
+const HeaderSection = styled(Section)`
+  max-width: 680px;
+`
+
 const DataPage = ({data, pageContext}) => {
-  let dataSource = data.allElectprojectCsv.nodes[0].source;
+  let MOST_RECENT = data.allElectprojectCsv.nodes[data.allElectprojectCsv.nodes.length-1];
+  let lastUpdate = moment(MOST_RECENT.report_date, 'M/D/YYYY').format('MMMM Do, YYYY');
+  // these dates come to us from ElectionProject, so we don't want to mess with their format until display
+  let dataSource = MOST_RECENT.source;
   let pageLink = data.ghostPage && data.ghostPage.slug;
 
   return (
     <Layout>
-      <Section id="Data__Section">
+      <HeaderSection>
         { pageLink && (
-          <h1><Link to={`/${pageLink}/`}>{pageContext.title} State Guide</Link></h1>
+          <Heading><Link to={`/${pageLink}/`}>{pageContext.title} Voting Data</Link></Heading>
         )}
-
+        <LastUpdated>Last updated: {lastUpdate}</LastUpdated>
+      </HeaderSection>
+      <Section id="Data__Section">
         <StateBar state={pageContext.state} title={`${pageContext.title} Ballots Cast`}
           electProject={data.allElectprojectCsv.nodes}
           population={data.allVepCsv.nodes}
@@ -25,9 +53,6 @@ const DataPage = ({data, pageContext}) => {
         <StateChart state={pageContext.state} title={`${pageContext.title} Vote Counts`}
           electProject={data.allElectprojectCsv.nodes}
         />
-      </Section>
-
-      <Section>
         <p>Data processed by Michael McDonald,
         <a href="https://electproject.github.io/Early-Vote-2020G/index.html">United States Elections Project, CC-SA 2020</a>.{' '}
         { dataSource.startsWith("http")
